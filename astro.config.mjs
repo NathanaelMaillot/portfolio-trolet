bef7c9bc386ce1f184d347d0b97fdd9bb267ed1a
NathanaelMaillot <nathanaelmaillot@trolet.ca>
2025-07-28 19:24:41 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://NathanaelMaillot.github.io',
+  base: '/portfolio-trolet',
   vite: {
     plugins: [tailwindcss()],
   },
